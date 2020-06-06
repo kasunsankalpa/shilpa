8164fb9ca4c919b3532cb6df6c9363c896564e50
kasunsankalpa <63305825+kasunsankalpa@users.noreply.github.com>
2020-06-06 20:05:07 +0530

    Update Grade.js

--- router/Grade.js
+++ router/Grade.js
@@ -8,4 +8,5 @@ router.post("/GetSubjectbyGrade_id",Grade.GetsubjectbyGradeId);
 router.post("/Getcource",Grade.getcource);
 router.post("/Getmodules",Grade.getmodule);
 router.post("/Getlesson",Grade.getlesson);
+router.post("/Getatachment",Grade.getatachment);
 module.exports = router;
